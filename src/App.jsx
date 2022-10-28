46b97c5fa55ea24ba45c3f47f26c3cb7cd22e9fc
Erick <kekierick15@gmail.com>
2022-10-28 14:12:11 -0300

    add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-function App() {
+import { Post } from "./Post";
+
+export function App() {
 
   return (
-    <h1> Hello World </h1>
+    <div>
+    <Post />
+    </div>
   )
 }
 
-export default App
